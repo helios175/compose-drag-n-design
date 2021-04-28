bb362edb681c8b938304403e202c229a8b5d1433
Helios Alonso Cabanillas <helios@squareup.com>
2021-04-28 11:09:37 -0700

    Linear base class

--- app/src/main/java/com/example/composehack/BuildScreen.kt
+++ app/src/main/java/com/example/composehack/BuildScreen.kt
@@ -1,37 +1,29 @@
 package com.example.composehack
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.IntrinsicSize.Min
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composehack.DraggableState.DRAGGABLE
 import com.example.composehack.DraggableState.NORMAL_DRAGGING
 
 @Composable
@@ -144,20 +136,29 @@ fun PreviewPlaceHolder() {
   }
 }
 
-data class Vertical(
-  val elements: List<Element>,
-  val extendFrom: Int,
-  val extendTo: Int
-) : Element {
+abstract class Linear() : Element {
+  abstract val elements: List<Element>
+  abstract val extendFrom: Int
+  abstract val extendTo: Int
+
+  abstract fun copyMySelf(
+    elements: List<Element>,
+    extendFrom: Int,
+    extendTo: Int
+  ): Linear
+
+  abstract fun Modifier.myDirectionMinSize(): Modifier
+  abstract fun Modifier.fillOtherDirection(): Modifier
+
   @Composable
   override fun generate(modifier: Modifier, onTransform: (Element) -> Unit) {
     Column(modifier = modifier
       .then(modifier)
     ) {
-      generate(modifier = Modifier.fillMaxWidth(), onTransform = onTransform)
+      generate(modifier = Modifier.fillOtherDirection(), onTransform = onTransform)
     }
   }
-  
+
   @Composable
   fun ColumnScope.generate(modifier: Modifier, onTransform: (Element) -> Unit) {
 
@@ -171,25 +172,24 @@ data class Vertical(
     ) {
       val childModifier = modifier
         .takeIf(extended) { weight(1f) }
-        .takeIf(!extended) { height(IntrinsicSize.Min) }
+        .takeIf(!extended) { myDirectionMinSize() }
       PlaceHolder(text)
         .generate(modifier = childModifier) { newElement ->
-        val newElements = elements.toMutableList().apply { add(index, newElement) }
-        onTransform(
-          this@Vertical.copy(
-            elements = newElements,
-            extendFrom = if (incrementExtendFrom) extendFrom + 1 else extendFrom,
-            extendTo = if (incrementExtendTo) extendTo + 1 else extendTo,
+          val newElements = elements.toMutableList().apply { add(index, newElement) }
+          onTransform(copyMySelf(
+              elements = newElements,
+              extendFrom = if (incrementExtendFrom) extendFrom + 1 else extendFrom,
+              extendTo = if (incrementExtendTo) extendTo + 1 else extendTo,
+            )
           )
-        )
-      }
+        }
     }
 
     @Composable
     fun placeElement(index: Int, extended: Boolean) {
       val childModifier = modifier
         .takeIf(extended) { weight(1f) }
-        .takeIf(!extended) { height(IntrinsicSize.Min) }
+        .takeIf(!extended) { myDirectionMinSize() }
       elements[index].generate(modifier = childModifier, onTransform = {})
     }
 
@@ -212,6 +212,19 @@ data class Vertical(
       placeHolder("bottom", index = index + 1, incrementExtendFrom = false, incrementExtendTo = false, extended = false)
     }
   }
+
+}
+
+data class Vertical(
+  override val elements: List<Element>,
+  override val extendFrom: Int,
+  override val extendTo: Int
+) : Linear() {
+  override fun copyMySelf(elements: List<Element>, extendFrom: Int, extendTo: Int) =
+    Vertical(elements, extendFrom, extendTo)
+
+  override fun Modifier.myDirectionMinSize() = height(IntrinsicSize.Min)
+  override fun Modifier.fillOtherDirection() = fillMaxWidth()
 }
 
 val sampleVertical = Vertical(
